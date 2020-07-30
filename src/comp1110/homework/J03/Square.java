534838038f34c8362c70dfe4bc836a0e9600fa6f
u7172618 <ming.lei@anu.edu.au>
2020-07-30 00:33:56 -0700

    Complete Homework J3

---
+++ src/comp1110/homework/J03/Square.java
@@ -0,0 +1,11 @@
+package comp1110.homework.J03;
+
+import java.util.Scanner;
+
+public class Square {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i = in.nextInt();
+        System.out.println(i*i);
+    }
+}
